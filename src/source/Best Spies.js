190bc6f2f4b59303db5601540e27f7f032061393
Yuwan Kumar <yuwankmr@gmail.com>
2023-03-11 11:18:17 +0530

    updated

---
+++ src/source/Best Spies.js
@@ -0,0 +1,52 @@
+export const csv = [
+	{
+		title: 'Johny English',
+		value: '10',
+		img: 'https://m.media-amazon.com/images/M/MV5BMTc0MTM3NzEzMl5BMl5BanBnXkFtZTcwNjE5MTU3NQ@@._V1_.jpg',
+	},
+	{
+		title: 'Harry Tasker',
+		value: '09',
+		img: 'https://wwwflickeringmythc3c8f7.zapwp.com/q:i/r:1/wp:1/w:371/u:https://cdn.flickeringmyth.com/wp-content/uploads/2021/05/true-lies-600x338.jpg',
+	},
+	{
+		title: 'Napoleon Solo',
+		value: '08',
+		img: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2020/12/Napoleon-Solo-The-Man-From-U.C.L.E.-67.jpg?q=50&fit=crop&w=767&dpr=1.5',
+	},
+	{
+		title: 'Illya Kuryakin',
+		value: '07',
+		img: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/07/Illya-Kuryakin-inThe-Man-From-U.N.C.L.E.jpg?q=50&fit=crop&w=767&dpr=1.5',
+	},
+	{
+		title: 'Nick Fury',
+		value: '06',
+		img: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/06/Nick-Fury-in-promo-for-The-Avengers.jpg?q=50&fit=crop&w=767&dpr=1.5',
+	},
+	{
+		title: 'Black Widow',
+		value: '05',
+		img: 'https://lumiere-a.akamaihd.net/v1/images/image_15671350.jpeg?region=0,0,640,480',
+	},
+	{
+		title: 'Harry Hart',
+		value: '04',
+		img: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2020/12/Harry-Hart-in-Kingsman-The-Golden-Circle.jpg?q=50&fit=crop&w=767&dpr=1.5',
+	},
+	{
+		title: 'William Brandt',
+		value: '03',
+		img: 'https://cdn.mos.cms.futurecdn.net/XgphwcNJfTVNAsVRgTVe8R-320-80.jpg',
+	},
+	{
+		title: 'Ethan Hunt',
+		value: '02',
+		img: 'https://culturedvultures.com/wp-content/uploads/2018/09/Ethan-Hunt.jpg',
+	},
+	{
+		title: 'James Bond',
+		value: '01',
+		img: 'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/03/james-bond-movies-v2.png',
+	},
+];
---
+++ src/source/mythical creatures.js
@@ -0,0 +1,152 @@
+export const csv = [
+	{
+		title: 'Bogeyman',
+		value: '',
+		img: 'https://i.pinimg.com/736x/86/72/3e/86723eb08db8aa11f05c1168d04106c5.jpg',
+	},
+	{
+		title: 'Vampire',
+		value: '',
+		img: 'https://play-lh.googleusercontent.com/R1dShZj1HNBQzrHhsAD9_F8BRc2FU-nlDMU7OIAIwnMge04QWvNZR-clnXnEd_v30Qo',
+	},
+	{
+		title: 'Dybbuk',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Dybbuk.jpg/1200px-Dybbuk.jpg',
+	},
+	{
+		title: 'Banshee',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Banshee.jpg/1200px-Banshee.jpg',
+	},
+	{
+		title: 'Pontianak',
+		value: '',
+		img: 'https://asiaghosts.com/wp-content/uploads/2021/12/22-1-1024x776.jpg',
+	},
+	{
+		title: 'Zombie',
+		value: '',
+		img: 'https://img.freepik.com/free-photo/scary-zombie-costume-cosplay_144627-14453.jpg?w=360',
+	},
+	{
+		title: 'Hydra',
+		value: '',
+		img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ88mhos42PTuAFB35jOGsXq6Cb9LTPv4bcONZsHUnCYZwiBw8J',
+	},
+	{
+		title: 'Chimera',
+		value: '',
+		img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQzqHSpNV16lszO8XxNwh6bq89b0AOKfphccrE8TBi7rcI207zO',
+	},
+	{
+		title: 'Bigfoot',
+		value: '',
+		img: 'https://news.rochesternh.gov/wp-content/uploads/2022/08/bigfoot-film-patterson-gimlin-696x392.webp',
+	},
+	{
+		title: 'Dragon',
+		value: '',
+		img: 'https://media.istockphoto.com/id/1138941725/photo/wyvern-3d-illustration.jpg?s=612x612&w=0&k=20&c=QzDHdL5BJ48THgOkRHbA3FbusXacQR_LidP5BhkAjfU=',
+	},
+	{
+		title: 'Unicorn',
+		value: '',
+		img: 'http://ghk.h-cdn.co/assets/16/13/2560x2560/square-1459268194-gettyimages-176570508.jpg',
+	},
+	{
+		title: 'Basilisk',
+		value: '',
+		img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4wUfSwDK8xRC_4fNDpSTWaWNNF1F21f4iH1csgjqgEr4RB0YZl9JZtTLKyVgCemCjpQc&usqp=CAU',
+	},
+	{
+		title: 'Phoenix',
+		value: '',
+		img: 'https://img.freepik.com/premium-photo/phoenix-bird-fire-mythological-fenix-bird-with-flames-fantasy-illustration_691560-3557.jpg?w=360',
+	},
+	{
+		title: 'Griffin',
+		value: '',
+		img: 'https://www.atshq.org/wp-content/uploads/2022/08/griffin.jpg',
+	},
+	{
+		title: 'Loch Ness Monster',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Hoaxed_photo_of_the_Loch_Ness_monster.jpg/230px-Hoaxed_photo_of_the_Loch_Ness_monster.jpg',
+	},
+	{
+		title: 'Werewolf',
+		value: '',
+		img: 'https://assets.reedpopcdn.com/werewolf-the-apocalypse-earthblood-review-enjoyable-but-scrappy-rpg-action-1612371947624.jpg/BROK/thumbnail/1200x600/werewolf-the-apocalypse-earthblood-review-enjoyable-but-scrappy-rpg-action-1612371947624.jpg',
+	},
+	{
+		title: 'Fauns',
+		value: '',
+		img: 'https://www.worldanvil.com/uploads/images/3e1e82f83d11080e7b7d8793cf404e08.jpg',
+	},
+	{
+		title: 'Centaurs',
+		value: '',
+		img: 'https://mythicalcreatures.edwardworthlibrary.ie/wp-content/uploads/2018/09/Fig.1.Le_Clerc_pl._facing_p.jpg',
+	},
+	{
+		title: 'Minotaur',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Tondo_Minotaur_London_E4_MAN.jpg/300px-Tondo_Minotaur_London_E4_MAN.jpg',
+	},
+	{
+		title: 'Aqrabuamelu',
+		value: '',
+		img: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8ab4c763-3396-465a-98f6-c8cb4cbd69d9/d5m1ydw-a787a4d0-c431-4230-9dbd-a6fc1a25aadb.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhhYjRjNzYzLTMzOTYtNDY1YS05OGY2LWM4Y2I0Y2JkNjlkOVwvZDVtMXlkdy1hNzg3YTRkMC1jNDMxLTQyMzAtOWRiZC1hNmZjMWEyNWFhZGIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ca2_y2hfhfRtxYNgJ0bFz3agAf2J8y-_fZ0yyZTKbnM',
+	},
+	{
+		title: 'Mermaid',
+		value: '',
+		img: 'https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_591/MTkwNTgxMTkwNzYyMDQ2NTg5/istock-mermaid-jpg.webp',
+	},
+	{
+		title: 'Gorgon / Medusa',
+		value: '',
+		img: 'https://i.pinimg.com/736x/52/01/4b/52014b96811b2713fe273e3513d58764--naga-greek-mythology.jpg',
+	},
+	{
+		title: 'Fairies',
+		value: '',
+		img: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/175361c3-62f5-4727-8a23-aa57af282b0c/dc33xpk-e735b139-9427-48b1-9144-725e07b3c4a4.png/v1/fill/w_622,h_350,q_70,strp/fairies_by_tjota_dc33xpk-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvMTc1MzYxYzMtNjJmNS00NzI3LThhMjMtYWE1N2FmMjgyYjBjXC9kYzMzeHBrLWU3MzViMTM5LTk0MjctNDhiMS05MTQ0LTcyNWUwN2IzYzRhNC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.sDBnjG5gnFkDdIJGUqXw7rShqVqnK41mEQi_AqS28VM',
+	},
+	{
+		title: 'Goblins',
+		value: '',
+		img: 'https://i0.wp.com/dmdave.com/wp-content/uploads/2020/02/shadow-goblin.png?fit=857%2C1200&ssl=1',
+	},
+	{
+		title: 'Gnomes',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/ML-W%C3%A4chter_%28cropped%29.JPG',
+	},
+	{
+		title: 'Leprechauns',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Leprechaun_ill_artlibre_jnl.png/640px-Leprechaun_ill_artlibre_jnl.png',
+	},
+	{
+		title: 'Ogre',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Lechatbotte4.jpg',
+	},
+	{
+		title: 'Cyclops',
+		value: '',
+		img: 'https://sites.google.com/site/fregaphysics/_/rsrc/1253222028233/science-in-the-media/strange-but-true/trueoriginofthecyclopsmyth/Polyphemus.gif?height=200&width=162',
+	},
+	{
+		title: 'Oni',
+		value: '',
+		img: 'https://img.freepik.com/free-vector/red-oni-mask-isolated-white_43623-920.jpg?w=2000',
+	},
+	{
+		title: 'Golems',
+		value: '',
+		img: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Prague-golem-reproduction.jpg',
+	},
+];
